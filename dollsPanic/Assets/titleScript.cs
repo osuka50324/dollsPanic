7968d9ace5d01a49c169691ee21d4b2286d53f0b
osuka50324 <osuka50324@gmail.com>
2018-05-17 18:34:11 +0900

    commit2

---
+++ dollsPanic/Assets/titleScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleScript : MonoBehaviour {
+
+    public SceneObject m_nextScene;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(m_nextScene);
+        }
+    }
+}
