8b4a3ee998a3d62a03d5d96eb781c7cf7fce7540
osuka50324 <osuka50324@gmail.com>
2018-05-26 14:48:35 +0900

    add StageSelect

---
+++ dollsPanic/Assets/script/stageSelect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stageSelect : MonoBehaviour {
+
+    [SerializeField]
+    int imageHeight;
+    [SerializeField]
+    int imageWidth;
+
+    [SerializeField]
+    SceneObject[] stgageScene;
+    
+    int currentStageNumber;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        currentStageNumber = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.RightArrow))
+        {
+            currentStageNumber++;
+            if(stgageScene.Length > currentStageNumber)
+            {
+                currentStageNumber = 0;
+            }
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentStageNumber--;
+            if (stgageScene.Length < 0)
+            {
+                currentStageNumber = stgageScene.Length - 1;
+            }
+        }
+    }
+}
---
+++ dollsPanic/Assets/script/title.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class title : MonoBehaviour {
+
+    [SerializeField]
+    SceneObject nextScene;
+
+    void Update()
+    {
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
