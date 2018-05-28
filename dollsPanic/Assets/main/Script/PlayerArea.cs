ecdfe3672b37d06ad03446fc3fd23c78c107ea4b
Rasis0923 <cipher17126@gmail.com>
2018-05-28 10:59:15 +0900

    tesuto

---
+++ dollsPanic/Assets/main/Script/PlayerArea.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArea : MonoBehaviour {
+    private Player PlayerScript;
+
+	// Use this for initialization
+	void Start () {
+        PlayerScript =  transform.parent.GetComponent<Player>();
+        GetComponent<MeshRenderer>().enabled = false;
+        transform.localScale *= 1.5f;
+        for(int i = 0; i < transform.childCount;)
+        {
+            Destroy(transform.GetChild(0).transform);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.parent.transform.position;
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Object")
+        {
+            PlayerScript.OnSelect(col.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Object")
+        {
+            PlayerScript.UnSelect(col.gameObject);
+        }
+    }
+}
