baa6468d34cac02eda01855e0f405aa43b241fbb
Unknown <theqsteer@yahoo.co.jp>
2018-05-31 18:09:29 +0900

    オプション画面のスクリプトを追加 ↑↓キーで選ぶ。Enterキーで決定。

---
+++ dollsPanic/Assets/main/Script/Menu/Game.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        transform.parent.parent.GetComponent<PauseScript>().GetObjectReturnGame(this.gameObject);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.parent.parent.GetComponent<PauseScript>().bDestroy[0])
+        {
+            Destroy(this);
+        }
+	}
+
+}
