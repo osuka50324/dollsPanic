9d0a75e10ef182444e06b4b430376da4582b5c9f
Unknown <osuka50324@gmail.com>
2018-06-18 09:54:29 +0900

    画面遷移追加

---
+++ dollsPanic/Assets/P_Oosuka/script/SingletonMonoBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance_;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance_ == null)
+            {
+                Type t = typeof(T);
+
+                instance_ = (T)FindObjectOfType(t);
+                if (instance_ == null)
+                {
+                    Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance_;
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        // 他のGameObjectにアタッチされているか調べる.
+        // アタッチされている場合は破棄する.
+        if (this != Instance)
+        {
+            Destroy(this);
+            //Destroy(this.gameObject);
+            Debug.LogError(
+                typeof(T) +
+                " は既に他のGameObjectにアタッチされているため、コンポーネントを破棄しました." +
+                " アタッチされているGameObjectは " + Instance.gameObject.name + " です.");
+            return;
+        }
+    }
+}
