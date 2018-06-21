d1587ecc37139e98e92aae245b546cc5d58c47e1
Rasis0923 <cipher17126@gmail.com>
2018-06-21 21:18:18 +0900

    カメラ制御

--- dollsPanic/Assets/P_Takeichi/main/Script/CameraMove.cs
+++ dollsPanic/Assets/P_Takeichi/main/Script/CameraMove.cs
@@ -7,13 +7,10 @@ public class CameraMove : MonoBehaviour
     private GameObject player;
     private const float MoveSpeed = 10;
     private const float RotateSpeed = 10;
-    private const int ChangeTime = 40;
-    private string PlayerName;
-    private int DeltaTime = 0;
+    private float DilayTime;
     // Use this for initialization
     void Start()
     {
-        PlayerName = player.name;
     }
 
     // Update is called once per frame
@@ -39,27 +36,22 @@ public class CameraMove : MonoBehaviour
             {
                 targetPosition += player.transform.forward;
                 targetPosition += player.transform.up;
+                DilayTime = 1.0f;
             }else
             {
                 break;
             }
         }
-
-        if (player.name != PlayerName)
-        {
-            DeltaTime = ChangeTime;
-        }
-        if (DeltaTime > 0)
+        
+        if (DilayTime > 0)
         {
             transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, MoveSpeed * (Time.deltaTime * 3.5f));
             transform.localRotation = Quaternion.Lerp(this.transform.localRotation, Quaternion.LookRotation(player.transform.position - this.transform.position), RotateSpeed * (Time.deltaTime * 3.5f));
-            DeltaTime--;
-            PlayerName = player.name;
+            DilayTime -= Time.deltaTime;
             return;
         }
         transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, MoveSpeed);
         transform.localRotation = Quaternion.Lerp(this.transform.localRotation, Quaternion.LookRotation(player.transform.position - this.transform.position), RotateSpeed);
-        PlayerName = player.name;
         //if (transform.position.z < 0)
         //{
         //    transform.position = new Vector3(10, 2, 0);
@@ -74,10 +66,6 @@ public class CameraMove : MonoBehaviour
     public void SetPlayer(GameObject P)
     {
         player = P;
-    }
-
-    public GameObject GetPlayer()
-    {
-        return player;
+        DilayTime = 1.0f;
     }
 }
