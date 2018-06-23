cfbc0bf31f72278e2a8cadf501e4c35466f127ad
Unknown <theqsteer@yahoo.co.jp>
2018-06-23 15:06:59 +0900

    Merge branch 'master' into Yamaguchi
    途中

---
+++ dollsPanic/Assets/P_Yamaguchi/Scan/Scan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan : MonoBehaviour {
+    private int g_nTime;
+	// Use this for initialization
+	void Start () {
+        g_nTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P) && g_nTime == 0)
+        {
+            GameObject Child = Instantiate(Resources.Load("Pause", typeof(GameObject))) as GameObject;
+        }
+	}
+}
