e89ca950057f9688b3cea67a3bb5e4ac65ee13cd
Rasis0923 <cipher17126@gmail.com>
2018-06-27 03:58:38 +0900

    音の実装の準備

---
+++ dollsPanic/Assets/P_Takeichi/Script/SEGroup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEGroup : MonoBehaviour {
+    public GameObject[] Audio;
+    private int Num = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetSE(AudioClip SE)
+    {
+        Audio[Num].GetComponent<AudioSource>().clip = SE;
+        Audio[Num].GetComponent<AudioSource>().Play();
+        Num++;
+        if(Num == Audio.Length)
+        {
+            Num = 0;
+        }
+    }
+}
--- dollsPanic/Assets/P_Takeichi/Script/SEManager.cs
+++ dollsPanic/Assets/P_Takeichi/Script/SEManager.cs
@@ -20,13 +20,46 @@ public class SEManager : MonoBehaviour {
     public AudioClip Extend;
     public AudioClip Fall;
     public AudioClip CatPunch;
+
+    public GameObject SoundGroup;
+    private SEGroup SEG;
+    private List<AudioClip> SE;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        SE.Add(Cancel);
+        SE.Add(GoriraPunch);
+        SE.Add(Jump);
+        SE.Add(Clear);
+        SE.Add(NotClear);
+        SE.Add(Sonar);
+        SE.Add(SonarHit);
+        SE.Add(HighJump);
+        SE.Add(PunchHit);
+        SE.Add(RailGo);
+        SE.Add(Cursor);
+        SE.Add(CatPunchHit);
+        SE.Add(Enter);
+        SE.Add(HighFall);
+        SE.Add(Extend);
+        SE.Add(Fall);
+        SE.Add(CatPunch);
+        SEG = SoundGroup.GetComponent<SEGroup>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void OnSE(string[] name)
+    {
+        foreach (var n in SE)
+        {
+            if (n.name.Equals(name))
+            {
+                SEG.SetSE(n);
+                break;
+            }
+        }
+    }
 }
