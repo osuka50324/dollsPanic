92030877e242c8e4ba291b5d6faebb7e0daf447f
Unknown <osuka50324@gmail.com>
2018-06-27 23:33:58 +0900

    リソース非同期読み込み＋ビルドセッティングテスト

--- dollsPanic/Assets/P_Oosuka/script/CommonFile.cs
+++ dollsPanic/Assets/P_Oosuka/script/CommonFile.cs
@@ -6,19 +6,20 @@ public class CommonFile : SingletonMonoBehaviour<CommonFile>
 {
     Sprite[,] sprits;
 
+    int index_;
+    int type_;
+
     protected override void Awake()
     {
         DontDestroyOnLoad(this);
-
         sprits = new Sprite[180, 3];
-        for (int j = 0; j < 3; j++)
-        {
-            for (int i = 0; i < 180; i++)
-            {
-                string pass = "animation" + j + "/result" + i;
-                StartCoroutine(LoadAsync(pass, i, j));
-            }
-        }
+    }
+
+    void Start()
+    {
+        index_ = 0;
+        type_ = 0;
+        StartCoroutine(LoadAsync());
     }
 
     public Sprite GetSprite(int index, int type)
@@ -48,10 +49,12 @@ public class CommonFile : SingletonMonoBehaviour<CommonFile>
         return sprits[index, type];
     }
 
-    private IEnumerator LoadAsync(string filePath, int index, int type)
+    private IEnumerator LoadAsync()
     {
+        string path = "animation" + type_ + "/result" + index_;
+
         //非同期ロード開始
-        ResourceRequest resourceRequest = Resources.LoadAsync<Texture2D>(filePath);
+        ResourceRequest resourceRequest = Resources.LoadAsync<Texture2D>(path);
 
         //ロードが終わるまで待機(resourceRequest.progressで進捗率を確認出来る)
         while (!resourceRequest.isDone)
@@ -61,6 +64,19 @@ public class CommonFile : SingletonMonoBehaviour<CommonFile>
 
         //ロード完了、resourceRequest.assetからロードしたアセットを取得
         Texture2D temp = resourceRequest.asset as Texture2D;
-        sprits[index, type] = Sprite.Create(temp, new Rect(0, 0, temp.width, temp.height), Vector2.zero);
+        sprits[index_, type_] = Sprite.Create(temp, new Rect(0, 0, temp.width, temp.height), Vector2.zero);
+
+        index_++;
+        if (index_ >= 179)
+        {
+            index_ = 0;
+            type_++;
+            if (type_ >= 3)
+            {
+                yield break;
+            }
+        }
+
+        StartCoroutine(LoadAsync());
     }
 }
